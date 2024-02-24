4178b89d10accb4f9391aac8cc51fa7f2497b038
chahat30 <chahatba9876@gmail.com>
2024-02-24 13:41:42 +0530

    Initial Setup

---
+++ src/constants/images.js
@@ -0,0 +1,15 @@
+import Green from "../assets/green.jpeg";
+import NotVerified from "../assets/not_verified.png";
+import Res3D from "../assets/Res3D.png";
+import Resto from "../assets/resto1.jpeg";
+import Scene27 from '../assets/Scene-27.jpg';
+import Scene43 from '../assets/Scene-43.jpg';
+import Spinner from '../assets/Spinner.gif';
+import VerifiedIcon from '../assets/verified-icon.gif';
+
+
+const images={
+    Spinner,VerifiedIcon,Scene43,Scene27, Resto, Res3D, NotVerified, Green
+};
+
+export default images;
